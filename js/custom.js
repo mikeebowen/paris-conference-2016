73c022885344a578ab345c32c78eee8e2abae632
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-02-10 15:22:34 -0800

    try something new

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,14 @@ function showMobile () {
   var windowWidth = window.innerWidth;
   var windowHeight = window.innerHeight;
 
+  var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+
+
   if (windowWidth > 1080) {
     for (var i = 0; i < laReserveLogoMobile.length; i++) {
       laReserveLogoMobile[i].style.display = 'none';
@@ -57,15 +65,26 @@ function showMobile () {
     }
   }
   
-  console.log('hola mundo 2\n', 'wrapperHeight', wrapperHeight, '\n windowHeight', windowHeight, '\n windowClientHeight', windowClientHeight, '\n screen.height', screen.height, '\n document.documentElement.clientHeight', document.documentElement.clientHeight);
+  console.log('hola mundo 2\n', 'wrapperHeight', wrapperHeight, '\n windowHeight', windowHeight, '\n windowClientHeight', windowClientHeight, '\n screen.height', screen.height, '\n document.documentElement.clientHeight', document.documentElement.clientHeight, '\nfooterHeight', footerHeight, 'height', height);
+  wrapper.style.height = '';
   //if the window is taller set the wrapper height to the window height
-  if (windowHeight > wrapperHeight) {
-    wrapper.style.height = (windowHeight - 80) + 'px';
+  if (height > wrapperHeight + 100) {
+    var diff = height - windowHeight + 60;
+    wrapper.style.height = (height - diff) + 'px';
+    // footer.style.position = 'fixed';
+    // footer.style.bottom = 0;
   } 
   // if the wrapper is taller reset the wrapper height value
-  if (windowHeight <= wrapperHeight) {
-    wrapper.style.height = (wrapperHeight - 80) + 'px';
+  if (height <= wrapperHeight + 100) {
+    wrapper.style.height = '100%';
+    // footer.style.position = 'relative';
+    // footer.style.bottom = 'auto';
     
+  }
+  var sum = 0;
+  for (var i = 0; i < document.getElementsByTagName('div').length; i++) {
+    sum += document.getElementsByTagName('div')[i].clientHeight
+    console.log(document.getElementsByTagName('div')[i].clientHeight, '\n', sum);
   };
 }
 
